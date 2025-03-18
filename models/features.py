aa3af0d959257e534e8a116df81c9636096b0735
Nathan Stene <nathanstene@gmail.com>
2025-03-18 10:38:23 -0400

    doc: finish features class documentation

--- models/features.py
+++ models/features.py
@@ -4,20 +4,58 @@ import typing as t
 
 
 class Features(np.ndarray):
-    def __new__(cls, input_array, *args, **kwargs):
-        # Convert input to ndarray
+    """A subclass of `numpy.ndarray` for handling and processing feature data.
+
+    This class extends `numpy.ndarray` to provide additional functionality for
+    feature processing, such as outlier removal, smoothing, and aggregation of
+    nightly features.
+
+    Attributes:
+        Inherits all attributes from `numpy.ndarray`.
+
+    Methods:
+        remove_outliers(): Removes outliers from the feature data.
+        smooth_features(): Applies smoothing to the feature data.
+        get_nightly_features(): Aggregates features over a nightly period.
+    """
+    def __new__(cls, input_array: np.ndarray[float], *args, **kwargs):
+        """Creates a new instance of the Features class.
+
+        Args:
+            input_array (array-like): Input data to be converted into a Features object.
+            *args: Additional positional arguments.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            Features: A new instance of the Features class.
+        """
         obj = np.asarray(input_array).view(cls)
         return obj
 
     def __array_finalize__(self, obj):
-        # This is called when creating a view of the object
+        """Finalizes the array creation process.
+
+        This method is called when creating a view of the object. It ensures that
+        any necessary attributes are copied over.
+
+        Args:
+            obj (numpy.ndarray): The original array being viewed.
+
+        Returns:
+            None
+        """
         if obj is None:
             return
-        # Copy any attributes if needed
         pass
 
     def remove_outliers(self):
-        """Remove outliers.
+        """Removes outliers from the feature data using the IQR method.
+
+        Outliers are identified using the interquartile range (IQR) and replaced
+        with `NaN` values. A rolling window is used to calculate the moving average
+        for outlier detection.
+
+        :return: The modified array with outliers removed.
         """
         window_size = 10
         nf = self.shape[0]
@@ -40,11 +78,16 @@ class Features(np.ndarray):
 
 
     def smooth_features(self):
-        """Smooth features."""
+        """Applies smoothing to the feature data using a rolling window.
+
+        A rolling window is used to calculate the moving average, and the features
+        are normalized by subtracting the mean and dividing by the standard deviation.
+
+        :return: The modified array with smoothed features.
+        """
         window_size = 3
         nf = self.shape[0]
         nch = self.shape[1]
-        ne = self.shape[2]
         for feat_idx in range(nf):
             for chan_idx in range(nch):
                 feat = self[feat_idx, chan_idx, :].copy()
@@ -59,10 +102,16 @@ class Features(np.ndarray):
 
 
     def get_nightly_features(self):
+        """Aggregates features over the whole recording using a rolling window.
+
+        A rolling window is used to calculate the moving average of the features,
+        and the normalized nightly features are computed.
+
+        :return: An array of shape (nf, nch) containing the aggregated nightly features.
+        """
         window_size = 10
         nf = self.shape[0]
         nch = self.shape[1]
-        ne = self.shape[2]
         nightly_features = np.zeros(shape=(nf, nch))
         for feat_idx in range(nf):
             for chan_idx in range(nch):
