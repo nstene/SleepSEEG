a052e780afa14e1522f6e80467647c264c521893
Nathan Stene <nathanstene@gmail.com>
2025-03-22 10:46:00 -0400

    test: arrange reader test

--- test/test_eeg_reader.py
+++ test/test_eeg_reader.py
@@ -1,35 +1,95 @@
 import os
 import unittest
 import numpy as np
-import h5py
+import tempfile
+import shutil
+
+import pyedflib
 
 from models.readers.edf_reader import EdfReader
 
 if os.getcwd().split('\\')[-1] == 'test':
     os.chdir("..")  # Go one directory up
 
+
+def create_sine_wave_edf(filepath, freq=10, duration=1, sampling_rate=512, amplitude=100):
+    """
+    Creates an EDF file with a single-channel sinusoidal wave.
+
+    - freq: Frequency of the sine wave in Hz
+    - duration: Length of the signal in seconds
+    - sampling_rate: Number of samples per second
+    - amplitude: Amplitude of the sine wave
+    """
+    num_samples = duration * sampling_rate
+    time = np.linspace(0, duration, num_samples, endpoint=False)
+    signal = amplitude * np.sin(2 * np.pi * freq * time)
+
+    digital_min = -32768
+    digital_max = 32767
+    signal_headers = [{
+        'label': 'EEG Fz-Cz',
+        'dimension': 'uV',
+        'sample_frequency': sampling_rate,
+        'physical_min': digital_min,
+        'physical_max': digital_max,
+        'digital_min': digital_min,
+        'digital_max': digital_max,
+        'transducer': '',
+        'prefilter': ''
+    }]
+
+    signal_scaled = np.round((signal / amplitude) * digital_max).astype(np.int32)
+
+    with pyedflib.EdfWriter(filepath, 1, file_type=pyedflib.FILETYPE_EDF) as f:
+        f.setSignalHeaders(signal_headers)
+        # f.writePhysicalSamples(signal)
+        f.writeDigitalSamples(signal_scaled)
+
+    return filepath, signal_scaled, sampling_rate
+
 class TestEdfReader(unittest.TestCase):
-    epoch_0_file = r'matlab_files/epoch_0_LTP1.mat'
-    full_data_file = r'eeg_data/auditory_stimulation_P18_002.edf'
 
     @classmethod
     def setUpClass(cls) -> None:
-        with h5py.File(cls.epoch_0_file, 'r') as file:
-            cls.epoch_0_matlab_data = list(file['x_save'])[0]
-        cls.edf = EdfReader(filepath=cls.full_data_file)
+        super().setUpClass()
+
+        cls.temp_folder = tempfile.TemporaryDirectory()
+        edf_file = "test.edf"
+        edf_path = os.path.join(cls.temp_folder.name, edf_file)
+        filepath, expected_signal, sampling_rate = create_sine_wave_edf(str(edf_path))
+        cls.edf = EdfReader(filepath=filepath)
+        cls.expected_signal = expected_signal
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        super().tearDownClass()
+
+        shutil.rmtree(cls.temp_folder.name)
 
     def test_extract_data_pyedflib(self):
-        window_center = self.edf.start_time_sample
-        window_length_seconds = 2.5
-        window_length_samples = window_length_seconds * self.edf.sampling_frequency
-        start_sample = window_center - round(window_length_samples / 2)
-        stop_sample = window_center + round(window_length_samples / 2 + 30 * self.edf.sampling_frequency)
-        data, _ = self.edf.extract_data_pyedflib(start_sample=start_sample, stop_sample=stop_sample)
+        data, _ = self.edf.extract_data_pyedflib(start_sample=0, end_sample=512, digital=True)
+        self.assertTrue(np.array_equal(self.expected_signal, data))
 
-        def rmse(signal1, signal2):
-            return np.sqrt(np.mean((signal1 - signal2) ** 2))
+class TestEdfMNEReader(unittest.TestCase):
 
-        error = rmse(self.epoch_0_matlab_data, data[0])
-        relative_rmse = error / (np.max(self.epoch_0_matlab_data) - np.min(self.epoch_0_matlab_data))
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
 
-        self.assertTrue(relative_rmse < 0.01)
+        cls.temp_folder = tempfile.TemporaryDirectory()
+        edf_file = "test.edf"
+        edf_path = os.path.join(cls.temp_folder.name, edf_file)
+        filepath, expected_signal, sampling_rate = create_sine_wave_edf(str(edf_path))
+        cls.edf = EdfReader(filepath=filepath)
+        cls.expected_signal = expected_signal
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        super().tearDownClass()
+
+        shutil.rmtree(cls.temp_folder.name)
+
+    def test_extract_data_mne(self):
+        data, _, _ = self.edf.load_data(start_sample=0, end_sample=512)
+        self.assertTrue(np.allclose(self.expected_signal, data, rtol=0.01))
